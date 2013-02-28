0b42721c49a684ca38c20d3ba00b7ffbb9153bf4
Zachery croasmun <th3p4ler1der@gmail.com>
2013-02-28 12:20:16 -0800

    phone number check function

--- Croasmun_z.js
+++ Croasmun_z.js
@@ -5,5 +5,44 @@ library
 */
 
 
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+
+
+
+
+var library = function (){
+	
+	//check phone number function
+	var phoneNum = function (val){
+		var phLen = val.length
+		for (var i=0; 1<=phLen; i++){
+			if (12 === phLen){
+			return true;
+				if (phLen = true && val.charAt(3) === "-"){       //Extra credit: nested conditional
+					return true;
+				}else{
+					return false;
+				}
+			}else {
+				return true;
+			}
+		}
+		
+		
+		
+	}
+	
+	return{
+		"phoneNum": phoneNum
+	}
+
+	
+	
+};
+
+
+var newLib = new library();
+
+console.log(newLib.phoneNum("509-237-4867"));
 
